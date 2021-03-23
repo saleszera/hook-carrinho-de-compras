2e3fb7a59b6a469050c0978fd2bd2f7872fca168
saleszera <crowzcoleslaw@gmail.com>
2021-03-23 20:31:34 -0300

    incrementando produto

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -36,7 +36,10 @@ const Cart = (): JSX.Element => {
     )
 
   function handleProductIncrement(product: Product) {
-    // TODO
+    updateProductAmount({
+      productId: product.id,
+      amount: product.amount + 1
+    });
   }
 
   function handleProductDecrement(product: Product) {
@@ -88,7 +91,7 @@ const Cart = (): JSX.Element => {
                   <button
                     type="button"
                     data-testid="increment-product"
-                  // onClick={() => handleProductIncrement()}
+                  onClick={() => handleProductIncrement(product)}
                   >
                     <MdAddCircleOutline size={20} />
                   </button>
